48bdb4d0e91b8407d6430f004089a47111cd6702
Inho Oh <inho.oh@sk.com>
2020-07-01 21:43:29 +0900

    add more matrix effects
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- src/matrix_test.c
+++ src/matrix_test.c
@@ -50,6 +50,16 @@ static void simple_set_pixel(void)
 
 static void simple_art(void)
 {
+	int i;
+	int count;
+	int dir = 0;
+
+	unsigned int rows[8] = {
+		0x00000000, 0x7e423800, 0x4224446c, 0x5a180492,
+		0x5a181800, 0x42241044, 0x7e420038, 0x00001000
+	};
+	unsigned int first, last;
+
 	/**
 	 * http://dotmatrixtool.com/
 	 *  - Width: 32
@@ -59,15 +69,93 @@ static void simple_art(void)
 	 */
 
 	ntoy_matrix_clear();
-	ntoy_matrix_set_row(0, 0x00000000);
-	ntoy_matrix_set_row(1, 0x7e423800);
-	ntoy_matrix_set_row(2, 0x4224446c);
-	ntoy_matrix_set_row(3, 0x5a180492);
-	ntoy_matrix_set_row(4, 0x5a181800);
-	ntoy_matrix_set_row(5, 0x42241044);
-	ntoy_matrix_set_row(6, 0x7e420038);
-	ntoy_matrix_set_row(7, 0x00001000);
-	ntoy_matrix_draw();
+
+	for (count = 1; count < 10000; count++) {
+		/* Draw */
+		for (i = 0; i < 8; i++)
+			ntoy_matrix_set_row(i, rows[i]);
+
+		ntoy_matrix_draw();
+		usleep(20 * 1000);
+
+		/* Rotate */
+		first = rows[0];
+		last = rows[7];
+		for (i = 0; i < 8; i++) {
+			if (dir == 0)
+				rows[i] = (rows[i] << 1) |
+					  ((rows[i] >> 31) & 0x01);
+			else if (dir == 1)
+				rows[i] = ((rows[i] & 0xFFFFFFFF) >> 1) |
+					  (rows[i] << 31);
+			else if (dir == 2) {
+				if (i < 7)
+					rows[i] = rows[i + 1];
+			} else if (dir == 3) {
+				if (i < 7)
+					rows[7 - i] = rows[7 - i - 1];
+			}
+		}
+
+		if (dir == 2)
+			rows[7] = first;
+		else if (dir == 3)
+			rows[0] = last;
+
+		if (count % 32 == 0) {
+			dir++;
+			if (dir > 3)
+				dir = 0;
+
+			printf("change dir: %d\n", dir);
+		}
+	}
+}
+
+static void _draw_bar(int index, int value)
+{
+	int y;
+	int flag;
+
+	for (y = 0; y < NTOY_NUM_ROWS; y++) {
+		if (8 - value > y)
+			flag = 0;
+		else
+			flag = 1;
+
+		/* width: 3 */
+		ntoy_matrix_set_pixel(y, index * 4, flag);
+		ntoy_matrix_set_pixel(y, index * 4 + 1, flag);
+		ntoy_matrix_set_pixel(y, index * 4 + 2, flag);
+
+		/* space */
+		ntoy_matrix_set_pixel(y, index * 4 + 3, 0);
+	}
+}
+
+static void simple_vertical_bar(void)
+{
+	int bar[8] = { 3, 2, 1, 4, 5, 6, 7, 8 };
+	int i;
+	int count;
+
+	ntoy_matrix_clear();
+
+	for (count = 1; count < 10000; count++) {
+		for (i = 0; i < 8; i++)
+			_draw_bar(i, bar[i]);
+
+		ntoy_matrix_draw();
+		usleep(50 * 1000);
+
+		for (i = 0; i < 8; i++) {
+			bar[i] += (rand() % 3 - 1);
+			if (bar[i] > 8)
+				bar[i] = 8;
+			if (bar[i] < 0)
+				bar[i] = 0;
+		}
+	}
 }
 
 static void simple_xy_loop(void)
@@ -78,10 +166,10 @@ static void simple_xy_loop(void)
 		for (j = 0; j < NTOY_NUM_COLS; j++) {
 			ntoy_matrix_set_pixel(i, j, 1);
 			ntoy_matrix_draw();
-			usleep(1 * 1000);
+			usleep(10 * 1000);
 			ntoy_matrix_set_pixel(i, j, 0);
 			ntoy_matrix_draw();
-			usleep(1 * 1000);
+			usleep(5 * 1000);
 		}
 	}
 
@@ -89,7 +177,7 @@ static void simple_xy_loop(void)
 		for (i = 0; i < NTOY_NUM_ROWS; i++) {
 			ntoy_matrix_set_pixel(i, j, 1);
 			ntoy_matrix_draw();
-			usleep(5 * 1000);
+			usleep(10 * 1000);
 			ntoy_matrix_set_pixel(i, j, 0);
 			ntoy_matrix_draw();
 			usleep(5 * 1000);
@@ -117,17 +205,22 @@ int main(int argc, char *argv[])
 
 	printf("\n\nstart test\n");
 
-	simple_set_row();
-	next_key();
+	if (argc == 2) {
+		printf("test = %d\n", argv[1][0] - '0');
 
-	simple_set_pixel();
-	next_key();
-
-	simple_art();
-	next_key();
-
-	simple_xy_loop();
-	next_key();
+		if (argv[1][0] == '0') {
+			simple_set_row();
+			next_key();
+			simple_set_pixel();
+			next_key();
+		} else if (argv[1][0] == '1') {
+			simple_xy_loop();
+		} else if (argv[1][0] == '2') {
+			simple_art();
+		} else if (argv[1][0] == '3') {
+			simple_vertical_bar();
+		}
+	}
 
 	ntoy_matrix_close();
 }
